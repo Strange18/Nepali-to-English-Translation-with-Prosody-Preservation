8b8ff8adc5350c1780e4932ced047c3a1f79789b
prashant <prashant.azminds@gmail.com>
2025-03-06 23:10:23 +0545

    some changes

--- src/server/puncuate.py
+++ src/server/puncuate.py
@@ -12,7 +12,7 @@ def setup_gemini_api(api_key: str):
 
 def add_punctuation(sentence: str) -> str:
     """Send a sentence to Gemini API and get a properly punctuated response."""
-    model = genai.GenerativeModel("gemini-1.5-pro")
+    model = genai.GenerativeModel("gemini-1.5-flash-002")
     prompt = (
         "You are an AI that trasnalates Nepali sentences into English sentences. "
         "Your task is to take a Nepali sentence and return "
